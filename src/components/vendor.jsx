31f6585164d9af3700da58a69055c57968c583f8
SDavidson1177 <SDavidson177@gmail.com>
2023-06-24 21:17:47 -0400

    Add filecoin api

--- src/components/vendor.jsx
+++ src/components/vendor.jsx
@@ -1,7 +1,31 @@
+// Import necessary packages
+import { HttpJsonRpcConnector, LotusClient } from "filecoin.js";
+import {useEffect} from 'react'
+
+// Use the local node URL to create a connection to your Lotus node
+const localNodeUrl = "/rpc/v0";
+const localConnector = new HttpJsonRpcConnector({ url: localNodeUrl });
+
+// // lotusClient exposes all Lotus APIs
+const lotusClient = new LotusClient(localConnector);
 
 // UI for vendor submit license as well as the evidence
 // for whatever service they provide
 export function VendorUI() {
+    const getVersion = async () => {
+        let version;
+        try {
+            version = await lotusClient.common.version();
+        } catch (e) {
+
+        }
+        console.log(version);
+    }
+    
+    useEffect(() => {
+        getVersion();
+    });
+
     return(
         <>
             <h1>Vendor UI</h1>
